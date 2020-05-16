7ed3ea4d7246547df04e1eb08bf2dbedc1fa71fa
Elite <Elite@DESKTOP-GV0NG19>
2020-05-16 23:52:15 +0530

    Add project files.

---
+++ Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMvcEFWebApi.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+
+        [Required]
+        [Column(TypeName ="nvarchar(200)")]
+        [Display(Name ="Full Name")]
+        public string FullName { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [Display(Name = "Emp Code")]
+        public string EMPCode { get; set; }
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string Mobile { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Position { get; set; }
+    }
+}
